078144f37f7a911795e2e83829700992bd7e4959
xingao <eexingao@qq.com>
2026-04-03 15:17:15 +0800

    add demo file

--- src/alpamayo1_5/helper.py
+++ src/alpamayo1_5/helper.py
@@ -22,7 +22,7 @@ import collections.abc
 
 MIN_PIXELS = 163840
 MAX_PIXELS = 196608
-BASE_PROCESSOR_NAME = "Qwen/Qwen3-VL-2B-Instruct"
+BASE_PROCESSOR_NAME = "/home/xingao/code/Alpamayo1.5/ckpts/Alpamayo-1.5-Qwen3-VL-8B"
 
 CAMERA_DISPLAY_NAMES = {
     0: "Front left camera",
