90fdc71ac490767568b22920211fc1428b491965
xingao <eexingao@qq.com>
2026-04-08 09:00:30 +0800

    update download files

--- tools/download_data.py
+++ tools/download_data.py
@@ -1,4 +1,5 @@
 from physical_ai_av.dataset import PhysicalAIAVDatasetInterface
+import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='download data')
@@ -7,7 +8,14 @@ parser.add_argument('--local_dir', type=str, help='download local_dir')
 
 args = parser.parse_args()
 
-chunk_id_list = args.chunk_id.split(',')
+if args.chunk_id == "-1":
+    chunk_id_list = np.arange(50, 200)
+else:
+    chunk_id_list = args.chunk_id.split(',')
+
+print(chunk_id_list)
+
+# chunk_id_list = args.chunk_id.split(',')
 local_dir = args.local_dir
 ds = PhysicalAIAVDatasetInterface(token=True, local_dir=local_dir)
 
@@ -24,18 +32,30 @@ for chunk_id in chunk_id_list:
         features=ds.features.LABELS.ALL
     )
 
-    ds.download_chunk_features(
-        int(chunk_id),
-        features=ds.features.RADAR.ALL
-    )
+    # ds.download_chunk_features(
+    #     int(chunk_id),
+    #     features=ds.features.RADAR.ALL
+    # )
 
     ds.download_chunk_features(
         chunk_id=int(chunk_id),
-        features=ds.features.CAMERA.ALL,
+        features=ds.features.CAMERA.CAMERA_CROSS_LEFT_120FOV,
+    )
+    ds.download_chunk_features(
+        chunk_id=int(chunk_id),
+        features=ds.features.CAMERA.CAMERA_FRONT_WIDE_120FOV,
+    )
+    ds.download_chunk_features(
+        chunk_id=int(chunk_id),
+        features=ds.features.CAMERA.CAMERA_CROSS_RIGHT_120FOV,
+    )
+    ds.download_chunk_features(
+        chunk_id=int(chunk_id),
+        features=ds.features.CAMERA.CAMERA_FRONT_TELE_30FOV,
     )
 
     # lidar data
-    ds.download_chunk_features(
-        int(chunk_id),
-        features=ds.features.LIDAR.LIDAR_TOP_360FOV
-    )
+    # ds.download_chunk_features(
+    #     int(chunk_id),
+    #     features=ds.features.LIDAR.LIDAR_TOP_360FOV
+    # )
